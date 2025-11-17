47961292a637fc033683b7a286f6a388ee442a90
Riyaz Haque <your-email@example.com>
2025-11-17 09:59:27 -0800

    Add device pool size

--- laghos.cpp
+++ laghos.cpp
@@ -248,9 +248,8 @@ int main(int argc, char *argv[])
    auto &rm = umpire::ResourceManager::getInstance();
    const char * allocator_name = "laghos_device_alloc";
    size_t umpire_dev_pool_size = ((size_t) dev_pool_size) * 1024 * 1024 * 1024;
-   size_t umpire_dev_block_size = 512;
-   //rm.makeAllocator<umpire::strategy::QuickPool>(allocator_name, rm.getAllocator("DEVICE"), umpire_dev_pool_size, umpire_dev_block_size);
-   rm.makeAllocator<umpire::strategy::QuickPool>(allocator_name, rm.getAllocator("DEVICE"));
+   size_t umpire_dev_block_size = 1024 * 1024;
+   rm.makeAllocator<umpire::strategy::QuickPool>(allocator_name, rm.getAllocator("DEVICE"), umpire_dev_pool_size, umpire_dev_block_size);
 
 #ifdef HYPRE_USING_UMPIRE
    HYPRE_SetUmpireDevicePoolName(allocator_name);
